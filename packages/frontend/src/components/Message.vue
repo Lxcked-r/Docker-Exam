250bd762761fb6c8df91d98a93f9256103dca0b0
Guillaume Ringoir <g.ringoir@rafisa.ch>
2024-02-13 11:08:35 +0100

    add message component

---
+++ packages/frontend/src/components/Message.vue
@@ -0,0 +1,21 @@
+<script setup>
+    defineProps({
+        text: String,
+        userID: String,
+        isOwnMessage: Boolean,
+        isLast: Boolean,
+    });
+</script>
+
+<template>
+    <div class="message">
+        <div class="message__content">
+            <div v-if="!isOwnMessage" class="flex items-end" >
+                <span class="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">{{ text }}</span>
+            </div>
+            <div v-else class="flex items-end justify-end">
+                <span class="px-4 py-2 rounded-lg inline-block bg-blue-500 text-white">{{ text }}</span>
+            </div>
+        </div>
+    </div>
+</template>
