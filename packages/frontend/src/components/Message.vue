b4b598b185b7e6c3894807d3674570153877279e
Guillaume Ringoir <g.ringoir@rafisa.ch>
2024-02-13 12:00:35 +0100

    fix messages with username

--- packages/frontend/src/components/Message.vue
+++ packages/frontend/src/components/Message.vue
@@ -1,15 +1,21 @@
 <script setup>
-    defineProps({
+    const props = defineProps({
         text: String,
         userID: String,
         isOwnMessage: Boolean,
         isLast: Boolean,
+        userName: String,
+        createdAt: String,
     });
 </script>
 
 <template>
     <div class="message">
-        <div class="message__content">
+        <div class="message_content">
+            <div v-if="!isOwnMessage" class="chat-header">
+                {{ userName }}
+                <time class="text-xs opacity-50">{{ createdAt }}</time>
+            </div>
             <div v-if="!isOwnMessage" class="flex items-end" >
                 <span class="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">{{ text }}</span>
             </div>
