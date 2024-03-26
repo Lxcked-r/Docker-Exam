39f855d1727f397a34685a50ad44c3f91bd6fa20
Guillaume Ringoir <g.ringoir@rafisa.ch>
2024-03-26 14:22:38 +0100

    better margin messages

--- packages/frontend/src/components/Message.vue
+++ packages/frontend/src/components/Message.vue
@@ -26,9 +26,8 @@ onBeforeMount(() => {
 <template>
     <div class="flex items-start gap-2.5]">
         <AvatarCircle v-if="isFirst" :id="userID" :force-fallback="true" :name="userName":avatar="avatar" @click="$emit('showUser')"/>
-        <li class="group/item flex flex-col leading-1.5 mb-2">
-                <span v-if="isFirst" class="text-sm font-semibold text-gray-900 dark:text-white">{{ userName }}<div v-if="isOwnMessage" class="text-sm text-500 dark:text-gray-400">(You)</div></span>
-                <span v-if="isFirst" class="text-sm font-normal text-gray-500 dark:text-gray-400">{{ createdAt }}</span>
+        <li class="group/item flex flex-col leading-1.5 mb-2 ml-2">
+                <span v-if="isFirst" class="text-sm font-semibold text-gray-900 dark:text-white">{{ userName + ' ' }}<span v-if="isFirst" class="text-sm font-normal text-gray-500 dark:text-gray-400">{{ createdAt }}</span></span>
             <p v-if="isFirst" :id="id" class="text-sm font-normal pt-1 mt-0 text-gray-900 dark:text-white max-w-[64rem]" @mouseover="$emit('showMessageOptions')">{{ text }}</p>
             <p v-else :id="id" class="text-sm font-normal text-gray-900 -mt-6 dark:text-white ml-[48px] max-w-[64rem]" @mouseover="$emit('showMessageOptions')">{{ text }}</p>
         </li>
